a8adcd10853096720235b1046050f7ceb7274f27
JongminAhn <160083809+JongminAhn@users.noreply.github.com>
2024-04-18 20:26:47 +0900

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Jongmin on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
